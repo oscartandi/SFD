8671c2b8cb865838a9fc6f5e4d0cc8fa833ec67c
Oscar Tandi <oscartandi171@gmail.com>
2023-11-02 23:58:06 +0530

    refined the code

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -1,17 +1,19 @@
 import pymongo
 from sensor.constant.database import DATABASE_NAME
+import certifi
+ca=certifi.where()
 
 class MongoDBClient:
-    clent=None
+    client=None
 
 
     def __init__(self, database_name= DATABASE_NAME) -> None:
         try:
-            if MongoDBClient.Client is None:
+            if MongoDBClient.client is None:
                 mongo_db_url="mongodb+srv://oscartandi171:8Gnar0uKkEcRH6Wm@mongosensor.hq9cx19.mongodb.net/"
-                MongoDBClient.client=pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
+                MongoDBClient.client=pymongo.MongoClient(mongo_db_url,tlsCAFile=ca)
             self.client=MongoDBClient.client
-            self.database = self.clie4nt[database_name]
+            self.database = self.client[database_name]
             self.dataase_name= database_name
         except Exception as e:
             raise e
